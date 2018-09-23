89ada6260ff95c414ea4ebcda04b1bd8b4a380bc
lucasdtownsend <lucasdtownsend@gmail.com>
2018-09-22 23:27:16 -0400

    updated with more pet rock jokes

--- VirtualPet/Program.cs
+++ VirtualPet/Program.cs
@@ -10,6 +10,81 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
+            VirtualPet vPet = new VirtualPet();
+            string input = "";
+            Console.WriteLine("Pet Rock Simulator 2018");
+            Console.WriteLine();
+            Console.WriteLine("Your Pet Rock is named Jeffrey. You love him very much.");
+            Console.ReadLine();
+            Console.WriteLine();
+            vPet.Hunger = 0;
+            vPet.Thirst = 0;
+            vPet.Waste = 0;
+            vPet.Boredom = 0;
+            vPet.Tired = 0;
+            vPet.Anger = 0;
+
+            while (input != "exit")
+            {
+                Console.WriteLine("JEFFREY");
+                Console.WriteLine("Hunger: " + vPet.Hunger);
+                Console.WriteLine("Thirst: " + vPet.Thirst);
+                Console.WriteLine("Waste: " + vPet.Waste);
+                Console.WriteLine("Boredom: " + vPet.Boredom);
+                Console.WriteLine("Exhaustion: " + vPet.Tired);
+                Console.WriteLine();
+                Console.WriteLine("Enter the number of what you would like to do.");
+                Console.WriteLine("1. Play With Your Pet Rock");
+                Console.WriteLine("2. Walk Your Pet Rock");
+                Console.WriteLine("3. Feed Your Pet Rock");
+                Console.WriteLine("4. Give Your Pet Rock Water");
+                Console.WriteLine("5. Have Your Pet Rock Take a Nap");
+                Console.WriteLine("Or type exit to close program.");
+                input = Console.ReadLine().ToLower();
+                Console.WriteLine();
+
+                if (input == "exit")
+                {
+                    return;
+                }
+                else if (input == "1")
+                {
+                    Console.WriteLine("You challenge your Pet Rock to a rousing game of Backgammon.");
+                    Console.ReadLine();
+                    vPet.Play();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("You put a leash on your Pet Rock and take to the streets, where it can do its disgusting business.");
+                    Console.ReadLine();
+                    vPet.Walk();
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("You give your Pet Rock a bowl a gravel to eat and mull the ethical ramifications.");
+                    Console.ReadLine();
+                    vPet.Feed();
+                }
+                else if (input == "4")
+                {
+                    Console.WriteLine("You drop your Pet Rock into some water and soon the water is gone. You ask no questions.");
+                    Console.ReadLine();
+                    vPet.Drink();
+                }
+                else if (input == "5")
+                {
+                    Console.WriteLine("You assume your Pet Rock is sleeping, but it's impossible to tell for sure.");
+                    Console.ReadLine();
+                    vPet.Sleep();
+                }
+                else
+                {
+                    Console.WriteLine("You stare at your Pet Rock and think about your life choices.");
+                    Console.ReadLine();
+                }
+
+                vPet.Tick();
+            }
         }
     }
 }
---
+++ VirtualPet/VirtualPet.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        // Fields
+        private int hunger;
+        private int thirst;
+        private int waste;
+        private int boredom;
+        private int tired;
+        private int anger;
+
+        // Properties
+        public int Hunger
+        {
+            get { return this.hunger; }
+            set { this.hunger = value; }
+        }
+
+        public int Thirst
+        {
+            get { return this.thirst; }
+            set { this.thirst = value; }
+        }
+        public int Waste
+        {
+            get { return this.waste; }
+            set { this.waste = value; }
+        }
+        public int Boredom
+        {
+            get { return this.boredom; }
+            set { this.boredom = value; }
+        }
+        public int Tired
+        {
+            get { return this.tired; }
+            set { this.tired = value; }
+        }
+        public int Anger
+        {
+            get { return this.anger; }
+            set { this.anger = value; }
+        }
+
+        // Constructor
+        public VirtualPet()
+        {
+
+        }
+
+        public void Tick()
+        {
+            hunger++;
+            thirst++;
+            waste++;
+            boredom++;
+            tired++;
+
+            if(tired >= 20)
+            {
+                Console.WriteLine("Your Pet Rock has collapsed in exhaustion. Little fella just needs a nap.");
+                Sleep();
+                tired++;
+                Console.ReadLine();
+            }
+            else if (thirst >= 15)
+            {
+                anger++;
+                Console.WriteLine("Your Pet Rock has resorted to drinking from the toilet like an uncivilized beast.");
+                Drink();
+                thirst++;
+                Console.ReadLine();
+            }
+            else if (hunger >= 15)
+            {
+                anger++;
+                Console.WriteLine("Desperate with hunger, your Pet Rock has broken into your pantry and eaten all your breakfast cereal.");
+                Feed();
+                hunger++;
+                Console.ReadLine();
+            }
+            else if (waste >= 10)
+            {
+                anger++;
+                Console.WriteLine("Your Pet Rock has made a mess on the floor. He is deeply ashamed.");
+                waste = 0;
+                Console.ReadLine();
+            }
+            else if (boredom >= 5)
+            {
+                anger++;
+                Console.WriteLine("Your Pet Rock made a passive aggressive Facebook post about how you never play with him.");
+                Console.ReadLine();
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void Feed()
+        {
+            if(hunger <= 0)
+            {
+                Console.WriteLine("But your Pet Rock won't eat.");
+                Console.ReadLine();
+            }
+            else
+            { 
+                hunger = -1;
+                thirst++;
+                waste = waste + 5;
+            }
+        }
+        
+        public void Drink()
+        {
+            if(thirst <= 0)
+            {
+                Console.WriteLine("But not this time. He must not be thirsty.");
+                Console.ReadLine();
+            }
+            else
+            {
+                thirst = -1;
+                waste++;
+            }
+        }
+
+        public void Play()
+        {
+            if(tired >= 15)
+            {
+                Console.WriteLine("But Jeffrey is too tired to play.");
+                Console.ReadLine();
+            }
+            else
+            {
+                boredom = -1;
+                hunger++;
+                thirst++;
+                tired = tired + 5;
+            }
+        }
+
+        public void Walk()
+        {
+            if (tired >= 10)
+            {
+                Console.WriteLine("But Jeffrey is too tired for a walk.");
+                Console.ReadLine();
+            }
+            else
+            {
+                waste = -1;
+                boredom = -1;
+                hunger = hunger + 5;
+                thirst = thirst + 5;
+                tired = tired + 10;
+            }
+        }
+
+        public void Sleep()
+        {
+            if(tired < 10)
+            {
+                Console.WriteLine("Your Pet Rock wasn't sleepy after all.");
+                Console.ReadLine();
+            }
+            tired = -1;
+            hunger++;
+            thirst++;
+        }
+
+
+    }
+}
